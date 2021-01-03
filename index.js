e087a4aac2767c857d326f7c5cac55423ed322d8
Michaela Wagner <mickywagner@gmail.com>
2021-01-02 17:02:27 -0700

    Create function validatePhoneNumber

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
+
+function validatePhoneNumber(num) {
+    const firstDigit = num[0]
+
+    if(firstDigit == 1 || firstDigit == 0) { 
+        return false
+    }
+    if(num.length !== 10) {
+        return false
+    }
+
+    return true
+}
+
+
 $('#contact-number').inputmask("(999) 999-9999");
