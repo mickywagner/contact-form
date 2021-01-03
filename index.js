a043617b4321ebe74c02ce99ea869c8bea53940e
Michaela Wagner <mickywagner@gmail.com>
2021-01-03 09:31:57 -0700

    Create function getPhoneDigitsOnly to get only numbers from form

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const phoneNumberEntryForm = document.querySelector("#phoneNumberEntryForm")
 phoneNumberEntryForm.addEventListener("submit", (e) => {
     e.preventDefault()
     const { phone } = e.target
-    console.log(phone.value)
+    const phoneDigitsOnly = getPhoneDigitsOnly(phone.value)
+
+    console.log(phoneDigitsOnly)
 })
+
+
 
 function validatePhoneNumber(num) {
     const firstDigit = num[0]
-
     if(firstDigit == 1 || firstDigit == 0) { 
         return false
     }
     if(num.length !== 10) {
         return false
     }
-
     return true
 }
 
+function getPhoneDigitsOnly(string) {
+    return string.replace(/[^0-9]/g, '')
+}
 
 $('#contact-number').inputmask("(999) 999-9999");
