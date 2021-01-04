a77c15e0934eb6fec1fdc26a34861560b4f75932
Michaela Wagner <mickywagner@gmail.com>
2021-01-04 11:34:58 -0700

    Remove spacing

--- formattedPhoneNumber.js
+++ formattedPhoneNumber.js
@@ -7,7 +7,5 @@ function formatPhoneNumber(tenDigitNunber = null) {
         throw new Error ("Phone number does not meet 10 digit required length")
     }
     const formattedPhoneNumber = `(${phoneNumberString.substr(0,3)}) ${phoneNumberString.substr(3, 3)}-${phoneNumberString.substr(6,4)}`
-
     return formattedPhoneNumber
 }
-
