edda4f50f641d9527ed967e65cb605be18886cf4
Michaela Wagner <mickywagner@gmail.com>
2021-01-04 11:32:44 -0700

    Add contingency for null or short number

--- formattedPhoneNumber.js
+++ formattedPhoneNumber.js
@@ -1,9 +1,13 @@
-function formatPhoneNumber(tenDigitNunber) {
+function formatPhoneNumber(tenDigitNunber = null) {
+    if(tenDigitNunber === null)  {
+        throw new Error ("No phone number available")
+    } 
     const phoneNumberString = tenDigitNunber.toString()
+    if(phoneNumberString.length < 10) {
+        throw new Error ("Phone number does not meet 10 digit required length")
+    }
     const formattedPhoneNumber = `(${phoneNumberString.substr(0,3)}) ${phoneNumberString.substr(3, 3)}-${phoneNumberString.substr(6,4)}`
 
     return formattedPhoneNumber
 }
 
-const formatted = formatPhoneNumber(2132589796)
-console.log(formatted)
